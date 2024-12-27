998e4ee3da77a34d48ca52d37f8fd4643a3fad2d
Yoan Lin <yoanlin068@gmail.com>
2024-12-28 00:22:40 +0800

    ESLint & Prettier Setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
 ];
 
 export default eslintConfig;
