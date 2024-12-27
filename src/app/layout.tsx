c6eeaa7e8e2718a39d92f2ecf6623c685f177566
Yoan Lin <yoanlin068@gmail.com>
2024-12-28 03:25:38 +0800

    Tailwind CSS Setup & Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bowlby_One_SC } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const bowlbyOneSC = Bowlby_One_SC({
+  variable: "--font-bowlbyOneSC",
+  weight: "400",
   subsets: ["latin"],
 });
 
@@ -24,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${bowlbyOneSC.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["selector"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +10,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
+        light: {
+          100: "D9D9D9",
+          200: "8A5E5E",
+        },
+        dark: {
+          100: "#DEDCDC", // font-color1
+          200: "#b0aeae", // font-color2
+          300: "#403837", // hover-color
+          400: "#292423", // dark-theme-color1
+          500: "#181313", // dark-theme-color2
+        },
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        bowlbyOneSC: "var(--font-bowlbyOneSC)",
       },
     },
   },
