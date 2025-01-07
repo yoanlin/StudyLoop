8e496db6837a1ddcfeb9addd08f60cd59103f39b
Yoan Lin <yoanlin068@gmail.com>
2025-01-07 15:12:06 +0800

    feat: Setup Github Google & OAuth + Adding Account Model

---
+++ src/auth.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import db from "./db/db";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+});
