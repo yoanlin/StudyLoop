1ceb81f459f90c99da3c41adf302d19eb02bb4c5
Yoan Lin <yoanlin068@gmail.com>
2025-02-06 21:00:59 +0800

    feat: enhance CommentCard with user profile links and rating display

--- src/components/cards/CommentCard.tsx
+++ src/components/cards/CommentCard.tsx
@@ -2,26 +2,34 @@ import Image from "next/image";
 import React from "react";
 import { GetCommentsOutput } from "../../../types/global";
 import { getTimeStamp } from "@/lib/utils";
+import Link from "next/link";
+import ROUTES from "../../../constants/routes";
 
 const CommentCard = ({ info }: { info: GetCommentsOutput }) => {
   return (
     <div className="flex w-full border-y px-5 py-8">
-      <Image
-        src={info.createdBy.image ? info.createdBy.image : "/fb-Avatar.png"}
-        alt="Avatar"
-        width={30}
-        height={30}
-        className="size-[30px] rounded-full object-cover"
-      />
+      <Link href={ROUTES.PROFILE(info.createdById)}>
+        <Image
+          src={info.createdBy.image ? info.createdBy.image : "/fb-Avatar.png"}
+          alt="Avatar"
+          width={30}
+          height={30}
+          className="size-[30px] rounded-full object-cover"
+        />
+      </Link>
+
       <div className="ml-3">
-        <p className="inline-block">
+        <Link href={ROUTES.PROFILE(info.createdById)}>
           {info.createdBy.name}
-          <span className="ml-3 text-base text-muted">
-            {getTimeStamp(info.createdAt)}
-          </span>
-        </p>
+        </Link>
+        <span className="ml-3 text-base text-muted">
+          {getTimeStamp(info.createdAt)}
+        </span>
         <p>{info.content}</p>
       </div>
+      <p className="ml-auto h-fit rounded-lg border p-4 font-bold shadow">
+        {info.rating}/10
+      </p>
     </div>
   );
 };
