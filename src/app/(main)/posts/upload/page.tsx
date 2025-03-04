54b457e389bfab8d7968a8df0f06e7bde6c0302f
Yoan Lin <yoanlin068@gmail.com>
2025-03-04 16:45:55 +0800

    feat: add authentication check on post upload page

--- src/app/(main)/posts/upload/page.tsx
+++ src/app/(main)/posts/upload/page.tsx
@@ -1,6 +1,11 @@
+import { auth } from "@/auth";
 import PostForm from "@/components/form/PostForm";
+import { redirect } from "next/navigation";
+import ROUTES from "../../../../../constants/routes";
 
 const UploadPost = async () => {
+  const session = await auth();
+  if (!session) redirect(ROUTES.LOG_IN);
   return (
     <div className="mt-8 pl-10">
       <h2 className="font-markaziText text-3xl font-black lg:ml-24">
